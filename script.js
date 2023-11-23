756bb5f8d84c3432c1f42ab4fd2fec303c816dc4
Roma-13 <romaasieshvili2007@gmail.com>
2023-11-23 22:09:41 +0400

    new changes

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeColor(){
+    var element = document.getElementById("word");
+    element.style.color = "red";
+}
+
+function makeItBlack(){
+    var element = document.getElementById("word");
+    element.style.color = "black";
+}
